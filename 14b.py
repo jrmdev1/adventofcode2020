8c5b48421c97b3fed224aa96aef706c8644ba6fb
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-08-25 12:24:51 -0500

    Add files via upload

--- 14b.py
+++ 14b.py
@@ -4,27 +4,34 @@
 import sys
 import re
 
+filename = "data14_short2.txt"
+
 # "{0:b}".format(10)
 #f'0b{number:08b}'
 
-def applyMask(mask, val):
-    newval = val
+# If the bitmask bit is 0, the corresponding memory address bit is unchanged.
+# If the bitmask bit is 1, the corresponding memory address bit is overwritten with 1.
+# If the bitmask bit is X, the corresponding memory address bit is floating.
+def applyMaskAndWrite(mask, address, val):
+    # OK, and handles both 1 and 0 address bits now.
+    newaddress = address
     mask1 = mask.replace("X", "0")
     mask1_int = int(mask1, 2)
     #print(f"mask1 binary = {mask1_int:b}")
-    newval |= mask1_int 
-    #print(f"mask1={mask1}, mask1_int={mask1_int}, newval={newval}")
+    newaddress |= mask1_int 
+    #print(f"mask1={mask1}, mask1_int={mask1_int}, newaddress={newaddress}")
 
-    mask0 = mask.replace("X", "1")
-    mask0_int = ~(int(mask0, 2))
+    #TODO: change to handle X now?
+    # LOOP through ALL X combinations and write
+    #mask0 = mask.replace("X", "1")
+    #mask0_int = ~(int(mask0, 2))
     #print(f"mask0 binary = {mask0_int:b}")
-    #print(f"mask0={mask0}, mask0_int={mask0_int}, newval={newval}")
-    newval &= ~mask0_int
+    #print(f"mask0={mask0}, mask0_int={mask0_int}, newaddress={newaddress}")
+    
+    #newaddress &= ~mask0_int
 
-    print(f"mask={mask}, val={val}, newval={newval}")
-    return newval
-
-filename = "data14_short.txt"
+    print(f"mask={mask}, address={address}, newaddress={newaddress},  val={val}")
+    return newaddress
 
 file = open(filename)
 filestr = file.read()
@@ -52,15 +59,15 @@ for line in a_list:
     elif line[:3] == "mem":
         #print(f"mem line: {line}")
         temp = line.split(" = ")
-        index_list_str = re.findall(r"\d+", temp[0])
-        index = int(index_list_str[0])
+        address_list_str = re.findall(r"\d+", temp[0])
+        address = int(address_list_str[0])
         val = int(temp[1])
-        print(f"index adress = {index} val={val}")
-        #newval = applyMask(mask, val)
-        newaddr = applyMask(mask, index)
+        print(f"address = {address} val={val}")
+        #newaddress = applyMask(mask, address)
+        newaddr = applyMaskAndWrite(mask, address, val)
         
-        #mem[index] = newval
-        mem[newaddr] = val
+        #mem[address] = newaddress
+        #mem[newaddr] = val  #TODO: DONT write here now...
 
         if newaddr > max_mem:
             max_mem = newaddr
