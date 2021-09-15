0d1f3412e95244a3b4264588b1705a679804b030
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-09-15 12:30:21 -0500

    Add files via upload

--- 15a.py
+++ 15a.py
@@ -11,6 +11,29 @@ a_list = filestr.split("\n")
 maxrows = len(a_list)
 print(a_list)
 #print(f"maxrows={maxrows}")
+guesses = [int(i) for i in a_list[0].split(",")]
+print(guesses)
+#guesses = dict(guesses)
+lastGuess = 0
+for i in range(len(guesses), 2019):
+    print(f"i={i}")
+    lastGuess = guesses[i-1]
+    n = guesses.count(lastGuess)
+    if n <= 1:
+        # first time for previous num
+        guesses.append(0)
+        print(f"added 0")
+    elif n>1:
+        # previously spoken
+        reverse = guesses[::-1]
+        for r, rval in enumerate(reverse):
+            # skip first
+            print(f"r={r}, rval={rval}")
+            if rval == lastGuess:
+                pass
+    else:
+        print(f"ERROR!, n == {n} lastGuess={lastGuess}")
+        break
 
-b_list = [int(i) for i in a_list[0].split(",")]
-print(b_list)
+
+print(f"i={i}, guesses[i]={guesses[i]}")
