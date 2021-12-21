b8da4e5279dd4cdb51289043a2fd4b56cd6e6249
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2021-12-21 15:19:39 -0600

    Add files via upload

--- 7a.py
+++ 7a.py
@@ -5,7 +5,7 @@
 # import re
 # import copy
 
-filename = "data7_short.txt"
+filename = "data7.txt"
 
 file = open(filename)
 filestr = file.read()
@@ -13,22 +13,25 @@ a_list = filestr.split("\n")
 maxrows = len(a_list)
 print(a_list)
 #maxcols = len(a_list[0])
-
 a_str = a_list[0].split(",")
 mylist = [int(elem) for elem in a_str]
 
-# numdays = 80
 maxpos = max(mylist)
 print(f"maxpos={maxpos}")
 
 print(f"{mylist}")
-# for day in range(1, numdays+1):
-#     for i in range(len(mylist)):
-#         if mylist[i] == 0:
-#             mylist[i] = 6
-#             mylist.append(8)
-#         else:
-#             mylist[i] -= 1
-#     #print(f"{day} : {mylist}")
 
-# print(f"total = {len(mylist)}")
+lowest_fuel = 10000000
+#lowest_pos = maxpos
+for hpos in range(1,maxpos+1):
+    per_pos_fuel = 0
+    for crab_pos in mylist:
+        fuelcost = max(crab_pos, hpos) - min(crab_pos, hpos)
+        #print(f"hpos: {hpos}, crab_pos: {crab_pos}, fuelcost: {fuelcost}")
+        per_pos_fuel += fuelcost
+    if per_pos_fuel < lowest_fuel:
+        lowest_fuel = per_pos_fuel
+        lowest_pos = hpos
+        print(f"updated low(pos,fuel): {lowest_pos}, {lowest_fuel}")
+    #print(f"hpos {hpos}, per_pos_fuel {per_pos_fuel}")
+print(f"lowest_pos = {lowest_pos}, lowest_fuel = {lowest_fuel}")
