13a2b2c458584676667daf0c6f6cf317fa17c9e3
Joseph McFarland <64599252+jrmdev1@users.noreply.github.com>
2022-07-06 10:23:18 -0500

    Add files via upload

--- 11a.py
+++ 11a.py
@@ -1,111 +1,68 @@
-#Advent of code 2020
-# 07/20/21 day 11a
-import copy
+#Advent of code 2021
+# 6/22/22 day 11a
+# Joe McFarland
+# import re
 
-filename = "data11.txt"
-
-file = open(filename)
-filestr = file.read()
-a_list = filestr.split("\n")
+filename = "data11_short2.txt"
+#a_list = open(filename).read().splitlines()
+a_list = open(filename).read().split()
 maxrows = len(a_list)
-maxcolumns = len(a_list[0])
-#print(a_list)
-print(f"maxrows={maxrows}, maxcolumns={maxcolumns}")
-#make 2d array
-matrix = []
-for i in range(maxrows):
-    matrix.append( list(a_list[i]))
-#print(f"2d:\n{matrix}")
+print(a_list)
+maxcols = len(a_list[0])
+print(f"maxrows = {maxrows}, maxcols={maxcols}")
 
-def Occupied( r, c ):
-    global matrix
-    global maxrows
-    global maxcolumns
-    #print(f"r={r},c={c}")
-    if r < 0 or r >= maxrows or c < 0 or c >= maxcolumns:
-        return False  # if exceeds bounds, then NOT occupied.
-    if matrix[r][c] == "#": 
-        return True
-    else:
-        return False # cannot be floor, or empty
+array = []
 
-# If a seat is empty (L) and 
-# there are no occupied seats adjacent to it, the seat becomes occupied
-def checkNoOccupiedSeatsAround( r, c ):
-    global matrix
-    #print(f"check around {r}, {c}")
-    for ri in range(-1,2):
-        for ci in range(-1,2):
-            if ri==0 and ci==0:
-                continue
-            if Occupied(r+ri, c+ci):
-                return False 
-    return True
+def flash(row, col):
+    rcoffset = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]]
+    for offsetsrow, offsetscol in rcoffset:
+        r = row+offsetsrow
+        c = col+offsetscol
+        if r < 0 or r >= maxrows or c < 0 or c >= maxcols:
+            print(f"flash exceeded, r={r}, c={c}")
+            return
 
-# If a seat is occupied (#) and four or more seats adjacent to it are also occupied, 
-# the seat becomes empty (L)
-# Otherwise, the seat's state does not change.
-def check4orMoreOccupied( r, c ):
-    global matrix
-    count = 0
-    if not Occupied(r,c):    # middle seat must be occupied!
-        return False
-    for ri in range(-1, 2):
-        for ci in range(-1, 2):
-            if ri==0 and ci==0:
-                continue
-            if Occupied(r + ri, c + ci):
-                count += 1
-                # middle seat must be occupied, so assume count of 4 or greater.
-                if count >= 4:
-                    #Can exit early, if already at count check.
-                    return True
-    return False
-  
-def countOccupied():
-    global matrix
-    cnt = 0
-    for row in matrix:
-        for char in row:
-            if char == "#":
-                cnt +=1
-    return cnt
+        array[r][c] += 1
+        if array[r][c] > 9:
+            flash(r, c)
+            array[r][c] = 0
+
+def dumpMatrix(octos):
+    for y in range(len(octos)):
+        for x in range(len(octos[0])):
+            print(octos[y][x],end='')
+        print()
+    print()
+
+for line in a_list:
+    row1 = [int(elem) for elem in line]
+    #print(f"row={row1}")
+    array.append(row1)
+print(f"array={array}")
+
+# for row in range(0, maxrows):
+#     for col in range(0, maxcols):
+#         energy = array[row][col]
+#         #print(f"{energy}")
+
+dumpMatrix(array)
+
+for step in range(0, 5):
+    for row in range(0, maxrows):
+        for col in range(0, maxcols):
+            #energy = array[row][col]
+            array[row][col] += 1
+            if array[row][col] > 9:
+                print(f"{row} {col} : exceeded: {array[row][col]}")
+
+                #flash(row, col)
+
+                array[row][col] = 0
+            #print(f"{energy}")
+    dumpMatrix(array)
+
+#dumpMatrix()
+dumpMatrix(array)
     
-changedmatrix = copy.deepcopy(matrix)
-#print(f"matrix={matrix}")
-#print(f"changedmatrix={changedmatrix}")
-needExit = False
-passNum = 0
-while not needExit:
-#for j in range(0,2):    # run just once or twice for test.
-    print(f"Pass #{passNum}:")
-    passNum += 1
-    for r, row in enumerate(matrix):
-        print(f"row: {row}")
-        #row_id = 0
-        for c, char in enumerate(row):
-            #print(f"c={c}, char={char}")
-            if char == ".":   #floor, skip it
-                continue
-            elif char == "L":    # empty, check around it.
-                if checkNoOccupiedSeatsAround( r, c ):
-                    changedmatrix[r][c] = "#"
-            elif char == "#":    # occupied, check around it.
-                if check4orMoreOccupied( r, c ):
-                    changedmatrix[r][c] = "L"
-            else:
-                print(f"INVALID CHAR!")
-                needExit = True
-                break
-        if needExit:
-            break
-    
-    if matrix == changedmatrix:
-        print(f"NO Changes. Pass complete!")
-        print(f"{matrix}")
-        cnt = countOccupied()
-        print(f"Num occupied = {cnt}")
-        needExit = True
-    else:
-        matrix = copy.deepcopy(changedmatrix)
-        print(f"Copied matrix from changedmatrix")
+
+#print(f"totalrisk = {totalrisk}")
